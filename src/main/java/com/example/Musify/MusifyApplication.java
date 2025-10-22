8bfa40a842ba05995fb5d53c1117a7cd6a1eac05
iamarnavtyagibhai-spec <iamarnavtyagibhai@gmail.com>
2025-10-23 00:41:53 +0530

    debugging

--- src/main/java/com/example/Musify/MusifyApplication.java
+++ src/main/java/com/example/Musify/MusifyApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MusifyApplication.class, args);
+		// SpringApplication.run(MusifyApplication.class, args);
+		 System.out.println("MONGODB_URI=" + System.getenv("MONGODB_URI"));
+        System.out.println("GOOGLE_CLIENT_ID=" + System.getenv("GOOGLE_CLIENT_ID"));
+        System.out.println("GOOGLE_CLIENT_SECRET=" + System.getenv("GOOGLE_CLIENT_SECRET"));
+        SpringApplication.run(MusifyApplication.class, args);
 	}
 
 }
