2e36528d79f072a22e14a205e2089446d63ee847
iamarnavtyagibhai-spec <iamarnavtyagibhai@gmail.com>
2025-10-23 01:01:11 +0530

    env removed

--- src/main/java/com/example/Musify/config/SecurityConfig.java
+++ src/main/java/com/example/Musify/config/SecurityConfig.java
@@ -5,10 +5,6 @@ package com.example.Musify.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -39,22 +35,5 @@ http
 return http.build();
 }
 
-@Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration googleRegistration = ClientRegistration.withRegistrationId("google")
-            .clientId(System.getenv("GOOGLE_CLIENT_ID"))
-            .clientSecret(System.getenv("GOOGLE_CLIENT_SECRET"))
-            .scope("openid", "profile", "email")
-            .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
-            .tokenUri("https://oauth2.googleapis.com/token")
-            .userInfoUri("https://openidconnect.googleapis.com/v1/userinfo")
-            .userNameAttributeName("sub")
-            .clientName("Google")
-            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-            .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-            .build();
 
-        return new InMemoryClientRegistrationRepository(googleRegistration);
-    }
-    
 }
